a750f82512cb15f28f4c8051ae4211bcaa780b4e
mumpo <sergiferriz@gmail.com>
2025-02-24 11:11:01 +0100

    nvim: navigate tabs

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -1,4 +1,9 @@
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
-map("n", "mj", ":m .+1<CR>==", { noremap = true, silent = false })
-map("n", "mk", ":m .-2<CR>==", { noremap = true, silent = false })
+map("n", "mj", ":m .+1<CR>==", opts)
+map("n", "mk", ":m .-2<CR>==", opts)
+
+-- Navigate buffers
+map("n", "<S-h>", ":bprevious<CR>", opts)
+map("n", "<S-l>", ":bnext<CR>", opts)
