0a7eba584c3d7b8dde7d16e042715b00426266fd
mumpo <sergiferriz@gmail.com>
2025-02-25 22:37:37 +0100

    nvim: add avante

---
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,19 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  build = "make",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    provider = "copilot",
+    windows = {
+      ask = {
+        border = "rounded",
+      },
+    },
+  },
+}
