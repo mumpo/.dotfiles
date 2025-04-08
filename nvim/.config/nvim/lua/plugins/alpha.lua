9baf6a1b5c8283c8b033ef6ffd1874d312304604
mumpo <sergiferriz@gmail.com>
2025-04-08 10:52:25 +0200

    nvim: add sky image in alpha

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -2,6 +2,7 @@ return {
   "goolord/alpha-nvim",
   event = "VimEnter",
   config = function()
+    require "alpha.term"
     local dashboard = require "alpha.themes.dashboard"
 
     local function dashboard_button(sc, txt, keybind)
@@ -10,15 +11,29 @@ return {
       button.opts.hl = "AlphaButtons"
       return button
     end
+    --
+    dashboard.section.terminal.command = "cat | ~/.config/nvim/ansi-images/sky.sh"
+    dashboard.section.terminal.width = 80
+    dashboard.section.terminal.height = 16
+    dashboard.section.terminal.opts.redraw = true
+
     dashboard.section.buttons.val = {
       dashboard_button("f", "  Find file", "<cmd>Telescope find_files<cr>"),
       dashboard_button("r", "  Recent files", "<cmd>Telescope oldfiles only_cwd=true<cr>"),
       dashboard_button("g", "  Grep", "<cmd>Telescope live_grep<cr>"),
       dashboard_button("q", "  Quit", "<cmd>qa<cr>"),
-      { type = "padding", val = 2 },
     }
     dashboard.section.buttons.opts.hl = "AlphaButtons"
-    dashboard.opts.layout[1].val = #dashboard.section.buttons.val
+    dashboard.section.buttons.opts.hl_shortcut = "AlphaShortcut"
+
+    dashboard.config.layout = {
+      { type = "padding", val = 1 },
+      dashboard.section.terminal,
+      { type = "padding", val = 1 },
+      dashboard.section.buttons,
+    }
+
+    -- print("dashboard.config" .. vim.inspect(dashboard.config))
 
     require("alpha").setup(dashboard.opts)
   end,
