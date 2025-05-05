7924ad27ac3a4ad1fdb7df66bdb8aadc501b496c
mumpo <sergiferriz@gmail.com>
2025-05-05 11:19:29 +0200

    macos: update config

--- macos/macos.sh
+++ macos/macos.sh
@@ -1,4 +1,9 @@
 # Change keyboard repeat rate (to move faster in nvim)
 # https://apple.stackexchange.com/questions/10467/how-to-increase-keyboard-key-repeat-rate-on-os-x
-defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write -g KeyRepeat -int 2 # normal minimum is 2 (30 ms)
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Disable automatic arrangement of spaces based on most recent use
+defaults write com.apple.dock "mru-spaces" -bool "false"
