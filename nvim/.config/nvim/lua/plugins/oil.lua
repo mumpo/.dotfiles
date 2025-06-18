e20f14add1afcb489f2d5bc35cfac84ff46427d0
Sergi Férriz Terensi <sergiferriz@gmail.com>
2025-06-18 15:59:26 +0200

    nvim: save confirmation in simple oil changes

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -4,6 +4,7 @@ return {
   ---@type oil.SetupOpts
   opts = {
     default_file_explorer = true,
+    skip_confirm_for_simple_edits = true,
   },
   dependencies = { "echasnovski/mini.icons" },
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
