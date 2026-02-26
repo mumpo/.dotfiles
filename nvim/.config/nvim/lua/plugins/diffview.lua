63545c9018975d2ff708cc76c9d4cf9776a3ceec
Sergi Férriz Terensi <sergiferriz@gmail.com>
2026-02-26 09:25:01 +0100

    nvim: update diffview keymaps

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -2,8 +2,9 @@ return {
   "sindrets/diffview.nvim",
   dependencies = "nvim-lua/plenary.nvim",
   keys = {
-    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
-    { "<leader>gD", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
-    { "<leader>gf", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
+    { "<leader>gdo", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+    { "<leader>gdc", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+    { "<leader>gdm", "<cmd>DiffviewOpen main<cr>", desc = "Open diffview comparing to main" },
+    { "<leader>gdf", "<cmd>DiffviewFileHistory %<cr>", desc = "File History" },
   },
 }
